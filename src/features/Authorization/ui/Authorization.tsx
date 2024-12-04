8a3ab6159712890a035152a3f0a9382bf258948b
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2024-12-04 21:46:46 +0300

    feat: [task] init

---
+++ src/features/Authorization/ui/Authorization.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/shared/ui/Button/Button';
+import { Input } from '../../../shared/ui/Input/Input';
+import styles from './Authorization.module.scss';
+
+export const Authorization = () => {
+  return (
+    <ul className={styles.container}>
+      <li className={styles.inputContainer}>
+        <p>Имя:</p>
+        <Input />
+      </li>
+      <li className={styles.inputContainer}>
+        <p>Почта:</p>
+        <Input />
+      </li>
+      <Button text='Отправить заявку' />
+    </ul>
+  )
+}
